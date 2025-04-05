39bd1ab885a171acc5e30e99cd7bb1e53e34a007
meraj2704 <mh1669101@gmail.com>
2025-04-05 15:18:24 +0600

    update code for the swager and user api created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       `mongodb+srv://atimeraj204:cOTajwDT4BfVs90L@cluster0.ztoo9j5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     ),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
